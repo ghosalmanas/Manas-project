2d81b731bc26440fd557f2c1bce22f2200253dc5
Manas <Manas@DESKTOP-9GBJAAB>
2018-12-09 13:03:14 +0530

    New addition

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value="\boot")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value="/")
+	public String getName() {
+		return "Its Hiland Woods";
+	}
+
+}
